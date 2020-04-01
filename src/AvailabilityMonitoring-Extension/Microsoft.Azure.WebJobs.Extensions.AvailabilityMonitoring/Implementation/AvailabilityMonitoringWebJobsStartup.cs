7c5322333597cb5bf51f3f70bf28eddd91b0ac04
macrogreg <macrogreg@live.com>
2020-04-01 12:49:54 -0700

    Added WebJobsStartup-attribute-based initialization.

---
+++ src/AvailabilityMonitoring-Extension/Microsoft.Azure.WebJobs.Extensions.AvailabilityMonitoring/Implementation/AvailabilityMonitoringWebJobsStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Azure.WebJobs.Extensions.AvailabilityMonitoring;
+using Microsoft.Azure.WebJobs.Extensions.AvailabilityMonitoring.Extensions;
+
+[assembly: WebJobsStartup(typeof(AvailabilityMonitoringWebJobsStartup))]
+
+namespace Microsoft.Azure.WebJobs.Extensions.AvailabilityMonitoring
+{
+    internal class AvailabilityMonitoringWebJobsStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddAvailabilityMonitoring();
+        }
+    }
+}
